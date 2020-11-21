e53da23d750122e19151959261714136140c8b39
Andrew Hancock <andrew06074@gmal.com>
2020-11-21 01:29:24 -0500

    release update for social media post

--- st_main.py
+++ st_main.py
@@ -9,19 +9,54 @@ import streamlit as st
 import seaborn as sns
 import numpy as np
 import matplotlib.ticker as ticker
+import matplotlib as mpl
 
+#authenticate with riot api activate lol watcher and set region to na1
+#global
+#load api key
+load_dotenv()
+#local key
+api_key = os.getenv('api_key')
+#heroku key
+#api_key = os.environ.get('api_key')
+watcher = LolWatcher(api_key)
+#set region to na1
+my_region = 'na1'
 
+def get_last_100_champs_played():
+    #get summoner id, needed to get matchlost
+    me = watcher.summoner.by_name(my_region,my_name)
+    #get matchlist
+    my_matches = watcher.match.matchlist_by_account(my_region,me['accountId'])
+    #get each champion played from last 100 games
+    #create blank list to store matchlist in
+    match_list = []
+    #returns last 100 champions as id, change id to name with Data Dragon
+    for row in my_matches['matches']:
+        match_list_row = {}
+        match_list_row['champion'] = row['champion']
+        match_list.append(match_list_row)
+    #get latest league version
+    latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
+    #get list of champion names from Data Dragon
+    champ_list = watcher.data_dragon.champions(latest,False,'en_US')
+    #blank dict for champin names and keys
+    champ_dict = {}
+    #load dict of champion names and ids as keys
+    for key in champ_list['data']:
+        row = champ_list['data'][key]
+        champ_dict[row['key']] = row['id']
+    #change id to champion name
+    for row in match_list:
+        row['championName'] = champ_dict[str(row['champion'])]
+    #create df from match list
+    df = pd.DataFrame(match_list)
+    #count number of occurances for each champion played
+    count = df.value_counts(df['championName'])
+
+    return count
 
 def get_info():
-    #global
-    #load api key
-    load_dotenv()
-    #local key
-    #api_key = os.getenv('api_key')
-    # heroku key
-    api_key = os.environ.get('api_key')
-    watcher = LolWatcher(api_key)
-    my_region = 'na1'
     #get summoner Id
     me = watcher.summoner.by_name(my_region,my_name)
     #get ranked stats
@@ -67,7 +102,8 @@ def get_win_loss_ratio_for_write_df(df):
 
 #USER INTERACTION HERE
 #get summoner name
-my_name = st.text_input('Enter your summoner name: ','Scarra')
+st.title('Search for summoner name: ')
+my_name = st.text_input('','Scarra')
 #when submit button is pressed
 if st.button('Submit'):
     with st.spinner("Search ongoing"):
@@ -75,28 +111,52 @@ if st.button('Submit'):
         df = get_info()
         #title for padding
         st.title('\n')
+        st.title('\n')
+        st.title('\n')
+        st.title('\n')
         #begin graph dev
         #revise df for graphing
         df_revised = prep_df_for_barchart()
         #set plotsize
-        fig, ax = plt.subplots(figsize=(7,10))
+        fig, ax = plt.subplots(figsize=(10,5))
         #color palette
         palette = {"wins":"green","losses":"red"}
         #set sns font_scale
-        sns.set(font_scale=1)
-        #seaborn style
+        sns.set(font_scale=2)
+        sns.set_style("white")
         #create
-        sns.set_style("whitegrid")
         ax = sns.barplot(y="queue_type",x="Value",hue="Category",data=df_revised,palette=palette,orient="h")
-        ax.set_xlabel('Count',fontsize=18)
-        ax.set_ylabel('Queue Type',fontsize=18)
-        ax.set_title('League of Legends - Season 13 Ranked Wins / Losses\n\n',fontsize=26)
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        ax.set_title(my_name + ' - Ranked Wins / Losses - Season 13'+'\n',fontsize=38)
         ax.xaxis.set_major_locator(ticker.MultipleLocator(20))
         ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+        #change tick params
+        ax.tick_params(axis='y', which='major', labelsize=28)
+        ax.tick_params(axis='x', which='major', labelsize=17)
+        ax.grid(False)
         st.pyplot(fig)
+        #format for table write
+        df['w_l'] = df['wins'] / (df['losses'] + df['wins'])
+        df.columns = ['Queue Type','Wins','Losses','Win Rate']
+        df.set_index('Queue Type', inplace=True)
+        st.table(df)
         #done creation
         st.title('\n')
-        #write df with win_loss_ratio
-        df = get_win_loss_ratio_for_write_df(df)
-        df.set_index(df.index,inplace=True)
-        st.write(df)
+        st.title('\n')
+        #pie plot
+        #get last 100 champs played
+        count = get_last_100_champs_played()
+        #top 10 for better pieplot formating
+        count = count.head(15)
+        labels = count.keys()
+        #set pieplot params
+        colors = sns.color_palette("tab10")
+        pie,ax = plt.subplots(figsize=[4,4])
+        plt.pie(x=count,labels=labels,textprops={'fontsize':10  },colors=colors)
+        ax.set_title(my_name + ' - Top 10 Champions Played - Season 13' + '\n',fontsize=12)
+        st.pyplot(pie)
+        count = count.reset_index(name='counts')
+        count.columns = ['Champion Name','Number of Games']
+        count.set_index('Champion Name', inplace=True)
+        st.table(count)
